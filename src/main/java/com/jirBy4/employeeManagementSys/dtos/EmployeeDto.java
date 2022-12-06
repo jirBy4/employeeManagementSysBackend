0dfdf5442700985ac033b86f1a4344381251896c
jirBy <jirby444@gmail.com>
2022-12-05 20:18:16 -0800

    Commit

--- src/main/java/com/jirBy4/employeeManagementSys/dtos/EmployeeDto.java
+++ src/main/java/com/jirBy4/employeeManagementSys/dtos/EmployeeDto.java
@@ -5,16 +5,23 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class EmployeeDto {
     private long id;
+    @NotEmpty(message = "Please enter a first name")
     private String firstName;
+    @NotEmpty(message = "Please enter a last name")
     private String lastName;
+    @NotEmpty(message = "Please enter an email")
     private String email;
+    @NotEmpty(message = "Please enter an age")
     private int age;
     private String favColor;
+    @NotEmpty(message = "Please enter a phone number")
     private Long phoneNo;
 }
